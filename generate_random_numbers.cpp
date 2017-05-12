1e84261fdb9936077939a2658aa4a8ea32ff6c03
matthewbaisden <mbaisden@student.rccd.edu>
2017-05-11 20:58:34 -0700

    Update generate_random_numbers.cpp

--- generate_random_numbers.cpp
+++ generate_random_numbers.cpp
@@ -1,8 +1,9 @@
 int generate_random(int min, int max)
 { 
-    static bool seed = true;
-    
-      srand(time(0));
-      return rand() % (max-min +1) +min;
+    static bool seed = true;+
+    if (seed) {srand (time(0)); seed = false;
+}
+   
+return rand() % (max-min +1) +min;
     
     
